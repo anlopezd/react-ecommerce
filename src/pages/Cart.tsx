97ce760701153e67f94f90d94decf2b59e8ac664
anlopezd <andres.lopez159@hotmail.com>
2023-01-06 12:43:34 -0500

    Popup implemented in cart page

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import BuyCart from "../components/popups/BuyCart";
 import { CartContext } from "../context/Cart";
 
 const Cart = () => {
   const { cart, dispatch } = useContext(CartContext);
+  const [ showPopUp, setShowPopUp ] = useState(false)
+  const [ showSuccessMessage, setShowSuccessMessage ] = useState(false)
 
   const increment = (id: number) => {
     dispatch({
@@ -25,8 +28,22 @@ const Cart = () => {
     });
   };
 
+ const startSuccessMessage = () => {
+    setShowSuccessMessage(true)
+    setTimeout(()=> setShowSuccessMessage(false), 2000)
+ }
+
   return (
     <>
+    {showSuccessMessage ? (
+      <div className="bg-black rounded text-white fixed top-5 px-3 py-1 z-50 left-1/2 transform translate-x-[-50%]">
+        Purchase made successfully!
+    </div>
+    ) : null}
+    <BuyCart setSuccessMessage={startSuccessMessage} show={showPopUp} closePopUp={()=> setShowPopUp(false)} />
+    
+
+
       <div className="w-full space-y-2">
         <div className="w-full grid grid-cols-5 px-10 border-b py-2">
           <p>Product</p>
@@ -85,7 +102,7 @@ const Cart = () => {
           <div></div>
           <div></div>
           <div></div>
-          <button className="bg-black px-2 py-2 text-white sm:w-28 mr-4 sm:mr-0">Buy</button>
+          <button onClick={()=> setShowPopUp(true)} className="bg-black px-2 py-2 text-white sm:w-28 mr-4 sm:mr-0">Buy</button>
         </div>
       </div>
     </>
