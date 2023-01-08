40fd2250431f2f73e7a5e778271a51623a56087f
anlopezd <andres.lopez159@hotmail.com>
2023-01-08 15:44:05 -0500

    Reducer removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
-import { ProductProvider } from "./context/Products";
+import { ProductProvider } from "./context/ProductsProvider";
 import Cart from "./pages/Cart";
-import { useReducer } from "react";
-import { ProductT } from "./interfaces";
-import { CartProvider } from "./context/Cart";
+import { CartProvider } from "./context/CartProvider";
 import Header from "./components/Header";
 
-const cartReducer = (
-  state: ProductT[],
-  action: {
-    type: "add" | "remove" | "increment" | "decrement" | "clear";
-    payload: ProductT | number;
-  }
-): ProductT[] => {
-  switch (action.type) {
-    case "add":
-      return [...state, action.payload as ProductT];
-    case "remove":
-      return state.filter(
-        (cartItem: ProductT) => cartItem.id !== action.payload
-      );
-
-    case "increment":
-      return state.map((cartItem: ProductT) =>
-        cartItem.id === action.payload
-          ? { ...cartItem, quantity: cartItem.quantity + 1 }
-          : cartItem
-      );
-    case "decrement":
-      return state.map((cartItem: ProductT) =>
-        cartItem.id === action.payload
-          ? { ...cartItem, quantity: Math.max(0, cartItem.quantity - 1) }
-          : cartItem
-      );
-
-    case "clear":
-      return [];
-
-    default:
-      break;
-  }
-
-  return state;
-};
-
-const initialState: ProductT[] = [];
-
 function App() {
-  const [cart, dispatch] = useReducer(cartReducer, initialState);
-
   return (
     <BrowserRouter>
-      <CartProvider cart={cart} dispatch={dispatch}>
+      <CartProvider>
         <ProductProvider>
           <Header />
           <Routes>
